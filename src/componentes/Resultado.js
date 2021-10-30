3b7425b9b0a09fadc1ea20a79b09f7f56d6b433b
luis rodriguez <kike.lerj2710@gmail.com>
2021-10-30 13:46:43 -0500

    terminando de acomodar el resultado a mostrar

--- src/componentes/Resultado.js
+++ src/componentes/Resultado.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const Resultado = () => {
-    return ( 
-        <div>desde Resultado</div>
-     );
-}
+const Resultado = ({ total, plazo, cantidad}) =>  ( 
+        <div className="u-full-with resultado">
+            <h2>Resumen</h2>
+            <p>la cantidad solicitada es: $ {cantidad}</p>
+            <p>Plazo a pagar en: $ {plazo} Meses</p>
+            <p>Pago mensual es: $ {(total/plazo).toFixed(2)}</p>
+            <p>total a pagar es de : $ {total} </p>
+        </div>
+);
+
  
 export default Resultado;
